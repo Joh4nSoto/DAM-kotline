0c89a96681e07ed8582a0508a5acabc644360a55
johan <joh.sotob@duocuc.cl>
2025-08-27 13:10:20 -0400

    clase 3

--- nullsafety.kt
+++ nullsafety.kt
@@ -38,4 +38,34 @@ fun main() {
         return "La edad de $edad años $msg de edad"
     }
     println(esmayor(18))
+    //EJERCICIO 1 saltar el 5 y entre el 11 al 19
+    for (i in 1..20) {
+        if (i == 5 || i in 11..19) {
+            continue  // Salta el número 5 y los del 11 al 19
+        }
+        println(i)
+    }
+    fun ciclo_do_while(){
+        var i = 1;
+        var ciclos =1;
+        do {
+
+            ciclos++;
+            val randomNumber= (0..10).random()
+            i = i + randomNumber
+            println("ciclo $ciclos : $i")
+
+        } while (i <=300)
+    }
+    ciclo_do_while()
+    fun dividir(a: Int, b:Int){
+        println("voy a dividir $a entre $b")
+        if( b != 0){
+            println(a / b)
+        }
+        else {
+            println("no se puede dividir por cero")
+        }
+    }
+    dividir(2,6)
 }
