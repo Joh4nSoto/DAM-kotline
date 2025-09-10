4e006df199ac5ddc2489ddf714225255bd8e9faf
johan <joh.sotob@duocuc.cl>
2025-09-09 21:46:32 -0300

    actualizacion del main

--- libreria/main.kt
+++ libreria/main.kt
@@ -6,37 +6,36 @@ import libreria.models.Libro
 import libreria.models.Categoria
 
 fun main(){
-    fun limpiar(){
-        Runtime.getRuntime().exec("cls")
-    }
-    fun opcion(){
-        var opcion_correcta = false
-        while(opcion_correcta!=true){
-            var opc = readln().toInt()
-            if(opc > 0 || opc < 6){
-                println("Ingresa un numero dentro del rango")
+    fun opcion():Int{
+        while(true) {
+            try {
+                var opc = readln().toInt()
+                return opc
+            } catch (e: Exception) {
+                // Code to handle the specific exception
+                println("Error inseperado: ${e.message}")
+                println("Ingrese nuevamente")
             }
         }
-
-
     }
+
     var encendido: Boolean = true
     var cc = CategoriaController()
     var lc = LibroController()
     cc.listarCategorias()
     while(encendido){
-        limpiar()
         println("---Bienvenido a la biblioteca---")
         println("1) Listar los libros")
         println("2) Agregar un libro")
         println("3) Editar un libro")
         println("4) Eliminar un libro")
         println("5) Salir")
-        var opc = readln().toInt()
-        if(opc==1){
-            println("Seleccionaste 1")
-            opc = readln().toInt()
-        }else if(opc==2){
+        if(opcion()==1){
+            println("Como deseas listarlos?")
+            println("1) Mostrar todos")
+            println("2) Filtrar por categoria")
+            opcion()
+        }else if(opcion()==2){
             println("Seleccionaste 2")
         }
     }
